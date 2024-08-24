012a9491f26de210b3c22f272ccd00d618947726
Shajedul Islam Shawon <54211870+shawondeveloper@users.noreply.github.com>
2024-08-24 23:14:36 +0600

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 
+import Link from 'next/link';
 import { usePathname } from 'next/navigation'
 export default function Home() {
   const  pathName = usePathname();
@@ -40,7 +41,7 @@ export default function Home() {
                         </div>
                         <button type="submit" className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign in</button>
                         <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                            Don’t have an account yet? <a href="/register" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign Up</a>
+                            Don’t have an account yet? <Link href="/register" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign Up</Link>
                         </p>
                     </form>
                 </div>
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Link from 'next/link';
 import { usePathname } from 'next/navigation'
 export default function Home() {
   const  pathName = usePathname();
@@ -43,7 +44,7 @@ export default function Home() {
                         </div>
                         <button type="submit" className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign in</button>
                         <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                            have an account? <a href="/" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign In</a>
+                            have an account? <Link href="/" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign In</Link>
                         </p>
                     </form>
                 </div>
